e98e41ea9e1e4b8bd0eafae98f0a5ba1054d5030
eduardogcapella <ecapellaturbo@gmail.com>
2021-10-12 14:31:52 -0300

    adicionando campo de nomes

--- livraria/urls.py
+++ livraria/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from rest_framework import routers
 from livros.api import viewsets as livroviewsets
@@ -27,4 +29,5 @@ route.register(r'livros', livroviewsets.LivrosViewSets, basename='Livros')     #
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(route.urls))
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- livros/models.py
+++ livros/models.py
@@ -4,6 +4,12 @@ from uuid import uuid4
 
 # Create your models here.
 
+
+'''função para fazer o upload das imagens. a instancia do objeto vai receber "instance" + o id_livro para não haver
+ problemas de ter dois nomes iguals, mais o nome do arquivo que vai receber o filename'''
+def upload_image_livros(instance, filename):
+    return f'{instance.id_livro}-{filename}'
+
 class Livros(models.Model):
     id_livro = models.UUIDField(primary_key=True, default=uuid4, editable=False)    # chave primaria com uuid
     titulo = models.CharField(max_length=255)
@@ -13,3 +19,4 @@ class Livros(models.Model):
     paginas = models.IntegerField()
     editora = models.CharField(max_length=255)
     criado_em = models.DateField(auto_now_add=True)     # auto adicionar a data que for criado dentro do campo.
+    image = models.ImageField(upload_to=upload_image_livros, blank=True, null=True) #blank vai receber true se nao quiser colocar imagens
